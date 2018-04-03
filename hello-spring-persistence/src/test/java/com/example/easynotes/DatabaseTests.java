5a79abbb01cebdb780d4074d11407ab86996deed
Dylan Nissley <dylan.nissley@gmail.com>
2018-04-03 06:32:18 -0500

    Move persistence project to a subdirectory

---
+++ hello-spring-persistence/src/test/java/com/example/easynotes/DatabaseTests.java
@@ -0,0 +1,15 @@
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+ 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PersistenceContext.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+public class ITFindBySearchTermTest {
+}
