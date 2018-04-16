365d6f7bfa8f61059fe47468b97839b1ace85127
Dylan Nissley <dylan.nissley@gmail.com>
2018-04-16 17:12:47 -0500

    Code for chapter 2 section 3 of spring in action, 5th edition meap

--- spring-in-action/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ spring-in-action/taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -4,9 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,7 +24,7 @@ import tacos.Ingredient.Type;
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
-  
+
   @GetMapping
   public String showDesignForm(Model model) {
     List<Ingredient> ingredients = Arrays.asList(
@@ -48,10 +52,15 @@ public class DesignTacoController {
   }
 
   @PostMapping
-  public String processDesign(Taco design) {
+  public String processDesign(@ModelAttribute("design") @Valid Taco design, Errors errors) {
+    if (errors.hasErrors()) {
+      return "design";
+    }
+
     // Save the taco design...
     // We'll do this in chapter 3
     log.info("Processing design: " + design);
+
     return "redirect:/orders/current";
   }
 
