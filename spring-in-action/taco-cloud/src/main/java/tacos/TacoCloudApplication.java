68bcaff62ed53c9dbc76785e21ff3085f2a14eb4
Dylan Nissley <dylan.nissley@gmail.com>
2018-04-19 16:06:22 -0500

    Code for chapter 2 section 4 of spring in action, 5th edition meap

--- spring-in-action/taco-cloud/src/main/java/tacos/TacoCloudApplication.java
+++ spring-in-action/taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -2,9 +2,16 @@ package tacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TacoCloudApplication {
+public class TacoCloudApplication implements WebMvcConfigurer {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("home");
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(TacoCloudApplication.class, args);
