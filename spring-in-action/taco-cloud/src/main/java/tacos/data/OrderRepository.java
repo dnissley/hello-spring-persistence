deded461e7e7010d13b64867844399eab58bbad8
Dylan Nissley <dylan.nissley@gmail.com>
2018-04-26 20:43:05 -0500

    Replace jdbc template usage with jpa/hibernate

--- spring-in-action/taco-cloud/src/main/java/tacos/data/OrderRepository.java
+++ spring-in-action/taco-cloud/src/main/java/tacos/data/OrderRepository.java
@@ -1,7 +1,16 @@
 package tacos.data;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
 import tacos.Order;
 
-public interface OrderRepository {
-  Order save(Order order);
+public interface OrderRepository 
+  extends CrudRepository<Order, Long> {
+
+  List<Order> findByZip(String zip);
+
+  List<Order> readOrdersByZipAndPlacedAtBetween(String zip, Date startDate, Date endDate);
 }
